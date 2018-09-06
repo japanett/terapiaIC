b9bf8cf4bf99a9b429b16a23ef205f65eabfbf8a
gabriel <gabriel.camargo@thinkdigital.com.br>
2018-09-06 10:13:11 -0300

    fix add pacient game bug

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -7,7 +7,7 @@ const user = mongoose.model('userSchema');
 const pacient = mongoose.model('pacientSchema');
 const game = mongoose.model('gameSchema');
 
-//OK
+
 exports.createPacient = async (data) => {
     var tempPacient = new pacient(data.pacient);
 
@@ -25,7 +25,7 @@ exports.createPacient = async (data) => {
         });
 }
 
-//OK
+
 exports.setPacientGame = async (data) => {
 
     var id = uuidv1();
@@ -33,6 +33,12 @@ exports.setPacientGame = async (data) => {
     var title;
     if (data.gameID === 1) {
         title = 'Jogo da maca'
+    }
+    else if (data.gameID === 2) {
+        title = 'Jogo do robo'
+    }
+    else if (data.gameID === 3) {
+        title = 'Jogo do Alecsander'
     }
     var __game = {
         pacient: data.identifier,
@@ -60,7 +66,6 @@ exports.setPacientGame = async (data) => {
         });
 }
 
-// OK, fazer validação da existencias jogo
 exports.deletePacientGame = async (data) => {
     pacient.findByIdAndUpdate(data.pacient, {
         $pull: { games: { 'idToPlay': data.gameid } }
@@ -73,7 +78,6 @@ exports.deletePacientGame = async (data) => {
         });
 }
 
-//OK
 exports.update = async (data) => {
     await user.findByIdAndUpdate(data.id,
         {
@@ -86,7 +90,6 @@ exports.update = async (data) => {
             rawResult: true
         });
 }
-//OK
 exports.updatePacient = async (data) => {
     await pacient.findOneAndUpdate({ identifier: data.identifier, medic: data.medic }, {
         $set: {
@@ -102,34 +105,34 @@ exports.updatePacient = async (data) => {
             rawResult: true
         });
 }
-//OK
+
 exports.createUser = async (data) => {
     var tempUser = new user(data);
     await tempUser.save();
 }
-//OK
+
 exports.get = async (data) => {
     const res = await user.findById(data);
     return res;
 }
-//OK
+
 exports.getPacients = async (data) => {
     const res = await pacient.find({ medic: data });
     return res;
 }
-//OK
+
 exports.getPacient = async (data) => {
     const res = await pacient.find({ medic: data.id, identifier: data.pacient_ident });
     return res;
 }
-//OK
+
 exports.delete = async (data) => {
     await user.findByIdAndRemove(data);
     await pacient.remove({ medic: data });
     await game.remove({ medic: data });
 }
 
-//OK
+
 exports.removePacient = async (data) => {
     //tratar o erro caso o usuario n exista, exemplo no createPacient
     await user.findByIdAndUpdate(data.id, {
@@ -148,7 +151,7 @@ exports.removePacient = async (data) => {
 }
 
 exports.getGameId = async (data) => {
-    const res = await game.findOne({ gameID: data, played: false });
+    const res = await game.findOne({ gameID: data.gameID, pacient: data.identifier, played: false });
     return res;
 }
 
