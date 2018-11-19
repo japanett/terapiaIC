23a48beb8b91a39b5732059022e387adf0e1a1c7
japanett <gabrielkenzo@gmail.com>
2018-11-19 10:44:53 -0200

    Version update

--- src/controller/index-controller.js
+++ src/controller/index-controller.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 
 exports.get = (req, res, next) => {
     res.status(200).send({
-        title: "API Fisioterapeuta",
-        version: "Versão: 1.1.0",
+        title: "API Lab Games VR",
+        version: "Versão: 2.0.0",
         author: 'Gabriel Kenzo Hirata Camargo'
     });
 }
