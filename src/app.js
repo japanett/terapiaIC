3a7237801ac3aff5ef414971057a1cd4ee49887a
Gabriel Kenzo Hirata Camargo <gabrielkenzo@gmail.com>
2019-03-01 18:33:39 -0300

    Cors Update
    allow patch http method

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ const userRoute = require('./routes/user-route');
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     next();
 });
 
