50c2acf88e0d00dc132658fc1e981201b10cdfa0
adrianorapussi <rapussi.adriano@gmail.com>
2019-08-14 20:58:43 -0300

    Adicao de arquivo de env e dependencia dotenv

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const encService = require('../src/services/enc-service');
 const logger = require('./winston');
+require('dotenv').config();
 
 global.KEY = 'gamesvr';
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL_CREATE_USER = 'Olá, <strong>name</strong><br>Você acabou de se cadastrar no <b>APP Games VR</b> !<br><br><strong>Usuário</strong>: login<br><strong>Senha</strong>: pwd';
 global.EMAIL_TMPL_CREATE_PACIENT = '<h2>Olá, username</h2><br><h2>Você acabou de cadastrar um paciente !</h2><br>Seguem os dados do mesmo: <br><br><strong>Identificador</strong>: identifier<br><strong>Nome</strong>: namepaciente<br><strong>Sexo</strong>: sexopaciente<br><strong>Idade</strong>: idadepaciente<br><strong>Patologia</strong>: patologiapaciente<br><strong>Objetivo</strong>: objetivopaciente<br><br><h3><u style="color:blue;">Lembrando que o paciente irá utilizar o Identificador para se logar no jogo !</u></h3>';
 
-const encPwd = '123G@bi123';
+const encPwd = process.env.CHAVE_ENCRPYT;
 const isProd = true;
 let prodData = {};
 
@@ -16,7 +17,7 @@ try {
     let decData = encService.decryptOld(encData, encPwd);
     prodData = JSON.parse(decData);
     if (isProd) {
-        prodData.connectionString = 'mongodb://admin:admin123@ds223542.mlab.com:23542/terapiaic';
+        prodData.connectionString = process.env.DB_URL;
     } else {
         prodData.connectionString = 'mongodb://localhost:27017/gamesvr';
     }
